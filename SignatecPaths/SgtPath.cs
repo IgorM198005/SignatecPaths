c9f91bb4ac2067a3e33cdd750c035886b14c95bc
im <IgorM198005@gmail.com>
2017-05-31 12:32:55 +0700

    11

---
+++ SignatecPaths/SgtPath.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SignatecPaths
+{
+    internal sealed class SgtPath
+    {
+        private const string absPathPattern = "^/([A-Za-z]+(/[A-Za-z]+)*)?$";
+
+        public const string InvalidArgumentFormatMessage = "Неверный формат аргумента";
+
+        public const string InvalidArgumentNestingMessage = "Уровень вложенности заданного аргументом превышает текущий";
+       
+        public SgtPath(string path)
+        {
+            if (!Regex.IsMatch(path, SgtPath.absPathPattern))
+            {
+                throw new ArgumentException("Неверный формат пути");
+            }
+
+            this.mCurrentPath = path;
+        }
+
+        private string mCurrentPath;
+
+        public string Cd(string cmd)
+        {
+            Match m;
+
+            if ((m = Regex.Match(cmd, @"^(?<up>\.\.(/\.\.)*)(?<newPath>(/[A-Za-z]+)*)$")).Success)
+            {
+                this.CombineUpPath(m);
+            }
+            else if ((m = Regex.Match(cmd, @"^(?<this>\.)(?<newPath>(/[A-Za-z]+)+)?$")).Success)
+            {
+                this.CombineDownPath(m);
+            }
+            else if (Regex.IsMatch(cmd, SgtPath.absPathPattern))
+            {
+                this.mCurrentPath = cmd;
+            }
+            else
+            {
+                throw new ArgumentException(SgtPath.InvalidArgumentFormatMessage);
+            }            
+
+            return this.mCurrentPath;
+        }
+
+        private void CombineUpPath(Match m)
+        {
+            var segments = this.mCurrentPath == "/" ? new string[] { string.Empty } : 
+                Regex.Split(this.mCurrentPath, "/");
+
+            int upToIdx = segments.Length -1 - Regex.Matches(m.Groups["up"].Value, "..").Count;
+
+            if (upToIdx < 0) throw new ArgumentException(SgtPath.InvalidArgumentNestingMessage);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i <= upToIdx; i++)
+            {
+                sb.Append("/");
+
+                sb.Append(segments[i]);
+            }
+
+            sb.Append(m.Groups["newPath"].Value);
+
+            this.mCurrentPath =  sb.ToString();
+
+            if (string.IsNullOrEmpty(this.mCurrentPath)) this.mCurrentPath = "/";
+        }
+
+        private void CombineDownPath(Match m)
+        {
+            if (m.Groups["newPath"].Success)
+            {
+                if (this.mCurrentPath == "/")
+                {
+                    this.mCurrentPath = m.Groups["newPath"].Value;
+                }
+                else
+                {
+                    this.mCurrentPath += m.Groups["newPath"].Value;
+                }
+            }
+        }
+    }
+}
---
+++ SignatecPaths/SgtPathTests.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SignatecPaths
+{
+    [TestFixture]
+    public class SgtPathTests
+    {
+        [Test]
+        public void InvalidEmptyCtorArg()
+        {
+            Assert.Throws<ArgumentException>(() => new SgtPath(""));
+        }
+
+        [Test]
+        public void InvalidFormatCtorArg1()
+        {
+            Assert.Throws<ArgumentException>(() => new SgtPath("abc"));
+        }
+
+        [Test]
+        public void InvalidFormatCtorArg2()
+        {
+            Assert.Throws<ArgumentException>(() => new SgtPath("//"));
+        }
+
+        [Test]
+        public void InvalidFormatCtorArg3()
+        {
+            Assert.Throws<ArgumentException>(() => new SgtPath("abcd/"));
+        }
+
+        [Test]
+        public void InvalidFormatCtorArg4()
+        {
+            Assert.Throws<ArgumentException>(() => new SgtPath("abc1"));
+        }
+
+        [Test]
+        public void InvalidEmptyCdArg()
+        {
+            var path = new SgtPath("/");
+
+            var ex = Assert.Throws<ArgumentException>(() => path.Cd(""));
+
+            Assert.That(ex.Message == SgtPath.InvalidArgumentFormatMessage, () => ex.Message);
+        }
+
+        [Test]
+        public void InvalidFormatCdArg1()
+        {
+            var path = new SgtPath("/");
+
+            var ex = Assert.Throws<ArgumentException>(() => path.Cd("../"));
+
+            Assert.That(ex.Message == SgtPath.InvalidArgumentFormatMessage, () => ex.Message);
+        }
+
+        [Test]
+        public void InvalidFormatCdArg2()
+        {
+            var path = new SgtPath("/");
+
+            var ex = Assert.Throws<ArgumentException>(() => path.Cd("//"));
+
+            Assert.That(ex.Message == SgtPath.InvalidArgumentFormatMessage, () => ex.Message);
+        }
+
+        [Test]
+        public void InvalidFormatCdArg3()
+        {
+            var path = new SgtPath("/");
+
+            var ex = Assert.Throws<ArgumentException>(() => path.Cd("abc/"));
+
+            Assert.That(ex.Message == SgtPath.InvalidArgumentFormatMessage, () => ex.Message);
+        }
+
+        [Test]
+        public void InvalidFormatCdArg4()
+        {
+            var path = new SgtPath("/");
+
+            var ex = Assert.Throws<ArgumentException>(() => path.Cd("/abc1"));
+
+            Assert.That(ex.Message == SgtPath.InvalidArgumentFormatMessage, () => ex.Message);
+        }
+
+        [Test]
+        public void InvalidFormatCdArg5()
+        {
+            var path = new SgtPath("/");
+
+            var ex = Assert.Throws<ArgumentException>(() => path.Cd("./"));
+
+            Assert.That(ex.Message == SgtPath.InvalidArgumentFormatMessage, () => ex.Message);
+        }
+
+        [Test]
+        public void InvalidFormatCdArg6()
+        {
+            var path = new SgtPath("/");
+
+            var ex = Assert.Throws<ArgumentException>(() => path.Cd("abc"));
+
+            Assert.That(ex.Message == SgtPath.InvalidArgumentFormatMessage, () => ex.Message);
+        }
+
+        [Test]
+        public void InvalidNestingCdArg()
+        {
+            var path = new SgtPath("/");
+
+            var ex = Assert.Throws<ArgumentException>(() => path.Cd(".."));
+
+            Assert.That(ex.Message == SgtPath.InvalidArgumentNestingMessage, () => ex.Message);
+        }
+
+        [Test]
+        public void CdUpToRootL1()
+        {
+            var path = new SgtPath("/abc");
+
+            Assert.AreEqual(path.Cd(".."), "/"); 
+        }
+
+        [Test]
+        public void CdUpToRootL2()
+        {
+            var path = new SgtPath("/abc/def");
+
+            Assert.AreEqual(path.Cd("../.."), "/");
+        }
+
+        [Test]
+        public void CdUpToRootL1DownF()
+        {
+            var path = new SgtPath("/abc");
+
+            Assert.AreEqual(path.Cd("../def"), "/def");
+        }
+
+        [Test]
+        public void CdUpToRootL2DownF()
+        {
+            var path = new SgtPath("/abc/def");
+
+            Assert.AreEqual(path.Cd("../../def"), "/def");
+        }
+
+        [Test]
+        public void CdUpTo1()
+        {
+            var path = new SgtPath("/abc/def");
+
+            Assert.AreEqual(path.Cd(".."), "/abc");
+        }
+
+        [Test]
+        public void CdUpTo2()
+        {
+            var path = new SgtPath("/abc/def/hgj");
+
+            Assert.AreEqual(path.Cd("../.."), "/abc");
+        }
+
+        [Test]
+        public void CdUpTo1AndDown()
+        {
+            var path = new SgtPath("/abc/def");
+
+            Assert.AreEqual(path.Cd("../wxy"), "/abc/wxy");
+        }
+
+        [Test]
+        public void CdUpTo2AndDown()
+        {
+            var path = new SgtPath("/abc/def/hgj");
+
+            Assert.AreEqual(path.Cd("../../wxy"), "/abc/wxy");
+        }
+
+        [Test]
+        public void CdDownTo0From0()
+        {
+            var path = new SgtPath("/");
+
+            Assert.AreEqual(path.Cd("."), "/");
+        }
+
+        [Test]
+        public void CdDownTo0From1()
+        {
+            var path = new SgtPath("/abc");
+
+            Assert.AreEqual(path.Cd("."), "/abc");
+        }
+
+        [Test]
+        public void CdDownTo1From0()
+        {
+            var path = new SgtPath("/");
+
+            Assert.AreEqual(path.Cd("./xyz"), "/xyz");
+        }
+
+        [Test]
+        public void CdDownTo1From1()
+        {
+            var path = new SgtPath("/abc");
+
+            Assert.AreEqual(path.Cd("./xyz"), "/abc/xyz");
+        }
+
+        [Test]
+        public void CdToAbs1()
+        {
+            var path = new SgtPath("/aBc");
+
+            Assert.AreEqual(path.Cd("/"), "/");
+        }
+
+        [Test]
+        public void CdToAbs2()
+        {
+            var path = new SgtPath("/");
+
+            Assert.AreEqual(path.Cd("/aBc/xyz"), "/aBc/xyz");
+        }
+
+        [Test]
+        public void CdToAbs3()
+        {
+            var path = new SgtPath("/abc/xyz");
+
+            Assert.AreEqual(path.Cd("/abcf/xyzf"), "/abcf/xyzf");
+        }
+    }
+        
+}
